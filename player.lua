7359c02e88f72c3123313576d7900d193fbb97ba
jxub <jjanarek@gmail.com>
2017-04-26 01:51:42 +0200

    remove timer, sprite animation, and heartbeat

--- player.lua
+++ player.lua
@@ -4,16 +4,14 @@ function player:new(x, y, height, width, speed)
   local p = {
       x = x,
       y = y,
-      height = height or 20,
+      height = height or 20, -- valor por defecto
       width = width or 20,
       speed = speed or 200,
       -- now the images... lots of them!
       standingRight = love.graphics.newImage('sprites/mario_standing_right.png'),
       movingRight = love.graphics.newImage('sprites/mario_moving_right.png'),
-      rightOther = love.graphics.newImage('sprites/mario_other_right.png'),
       standingLeft = love.graphics.newImage('sprites/mario_standing_left.png'),
       movingLeft = love.graphics.newImage('sprites/mario_moving_left.png'),
-      leftOther = love.graphics.newImage('sprites/mario_other_left.png'),
       -- and the movement flags
       left = true,
       right = false,
@@ -30,22 +28,22 @@ end
 function player:update(dt)
   -- movimiento del jugador
   -- change up key to only pressed once https://love2d.org/wiki/love.keypressed
-  if love.keyboard.isDown('up' or 'w')then
+  if love.keyboard.isDown('up')then
     io.write("key pressed: up\n")
     self.y = self.y - self.speed * dt
   end
   -- solo podemos movernos abajo si no estamos en una plataforma
-  if love.keyboard.isDown('down' or 's') and self.grounded == false then
+  if love.keyboard.isDown('down') and self.grounded == false then
     io.write("key pressed: down\n")
     self.y = self.y + self.speed * dt
   end
-  if love.keyboard.isDown('left' or 'a') then
+  if love.keyboard.isDown('left') then
     io.write("key pressed: left\n")
     self.x = self.x - self.speed * dt
     self.left = true
     self.right = false
   end
-  if love.keyboard.isDown('right' or 'd') then
+  if love.keyboard.isDown('right') then
     io.write("key pressed: right\n")
     self.x = self.x + self.speed * dt
     self.right = true
@@ -55,36 +53,21 @@ function player:update(dt)
   if self.grounded == false then
     -- tenemos que disminuir la gravedad para que los saltos sean dinámicos,
     -- si no hay aceleración
-    if love.keyboard.isDown('up' or 'w') then
+    if love.keyboard.isDown('up') then
       self.y = self.y + 30 * dt
     else
       self.y = self.y + 70 * dt
     end
   end
-  -- actualizamos el pulso del jugador cada n frames
-  self.timer = self.timer + 1
-  if self.timer >= 10 then
-    self.heartbeat = true
-    self.timer = 0
-  end
-  io.write("self.timer: "..self.timer.." dt: "..dt.."\n")
 end
 
 function player:draw()
   -- si está en tierra
   if self.grounded == true then
-    if self.heartbeat == true then
-      if self.left == true then
-        love.graphics.draw(self.standingLeft, self.x, self.y, nil, 0.3, 0.3)
-      elseif self.right == true then
-        love.graphics.draw(self.standingRight, self.x, self.y, nil, 0.3, 0.3)
-      end
-    elseif self.heartbeat == false then
-      if self.left == true then
-        love.graphics.draw(self.leftOther, self.x, self.y, nil, 0.3, 0.3)
-      elseif self.right == true then
-        love.graphics.draw(self.rightOther, self.x, self.y, nil, 0.3, 0.3)
-      end
+    if self.left == true then
+      love.graphics.draw(self.standingLeft, self.x, self.y, nil, 0.3, 0.3)
+    elseif self.right == true then
+      love.graphics.draw(self.standingRight, self.x, self.y, nil, 0.3, 0.3)
     else
       io.write("Error!!!\n")
     end
@@ -92,16 +75,20 @@ function player:draw()
   elseif self.grounded == false then
     if self.left == true then
       love.graphics.draw(self.movingLeft, self.x, self.y, nil, 0.3, 0.3)
-    else
+    elseif self.right == true then
       love.graphics.draw(self.movingRight, self.x, self.y, nil, 0.3, 0.3)
+    else
+      io.write("Error!!!\n")
     end
   end
 end
 
+-- función que nos escribe las coordenadas del jugador en la consola
 function player:log()
   io.write("player position is: "..self.x..", "..self.y.."\n")
 end
 
+-- nos da la coordenada y inferior del jugdor dependiendo de la imagen
 function player:bottom()
   if self.grounded == true then
     if self.left == true then

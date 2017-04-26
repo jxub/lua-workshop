708f5cd50143a7d2d454b94400db57dfe1682c12
jxub <jjanarek@gmail.com>
2017-04-26 12:14:03 +0200

    add comment

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
   t.title = "Platform game!" -- nombre de la ventana
   t.version = "0.10.2" -- versión del love que usamos
-  t.console = true
+  t.console = true -- los errores salen en la consola
 end
